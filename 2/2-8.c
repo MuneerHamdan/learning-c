4b558d9eaf1e5313148e85780bf5724879b6f990
MuneerHamdan <muneerhamdan01@gmail.com>
2026-08-30 08:25:10 -0400

    2-8

---
+++ 2/2-8.c
@@ -0,0 +1,41 @@
+/*
+ * 2-8
+ *
+ * Write a program that calculates the remaining balance on a loan after the first, second, and third monthly payments:
+ * Enter amount of loan: 20000.00
+ * Enter interest rate: 6.0
+ * Enter monthly payment: 386.66
+ * Balance remaining after first payment: $19713.34
+ * Balance remaining after second payment: $19425.25
+ * Balance remaining after third payment: $19135.71
+ * Display each balance with two digits after the decimal point. Hint: Each month, the balance is decreased by the amount of the payment, but increased by the balance times the monthly interest rate. To find the monthly interest rate, convert the interest rate entered by the user to a percentage and divide it by 12.
+ */
+
+#include <stdio.h>
+int main(void) {
+  float loan, interest, monthly;
+  printf("Enter amount of loan: ");
+  fscanf(stdin, "%f", &loan);
+  printf("Enter interest rate: ");
+  fscanf(stdin, "%f", &interest);
+  printf("Enter monthly payment: ");
+  fscanf(stdin, "%f", &monthly);
+
+  interest /= (100.0f*12.0f);
+  for (int i = 0; i < 3; i++){
+    loan -= monthly;
+    loan += (loan+monthly)*(interest);
+    switch (i){
+      case 0:
+        printf("Balance remaining after first payment: $%.2f\n", loan);
+        break;
+      case 1:
+        printf("Balance remaining after second payment: $%.2f\n", loan);
+        break;
+      case 2:
+        printf("Balance remaining after third payment: $%.2f\n", loan);
+        break;
+    }
+  }
+  return 0;
+}
